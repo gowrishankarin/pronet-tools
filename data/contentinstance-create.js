43e3b3fa3c5a9f07f5d6aba508bd287913efdced
Gowri Shankar <gowrishankar.m@prodapt.com>
2014-11-07 16:33:24 +0530

    Sample drools & data models

--- data/contentinstance-create.js
+++
@@ -1,9 +0,0 @@
-{
-	"contentInstanceId" : "",
-	"creationTime" : null,
-	"lastModifiedTime" : null,
-	"contentTypes" : null,
-	"mimeType" : null,
-	"contentSize" : 0,
-	"content" : "ABCD-THIS-IS-BASE64-ENCODED-STRING-VXYZ"
-}
